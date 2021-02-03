28bce42d469486411faa548eb4894ace96086bf6
Spectrum <53658189+SpectrumFATM@users.noreply.github.com>
2021-02-03 15:46:27 +0000

    Delete MissysDelight-master directory

--- MissysDelight-master/src/main/java/thearknoah05/missy/other/IMakeItem.java
+++
@@ -1,9 +0,0 @@
-package thearknoah05.missy.other;
-
-import net.minecraft.item.Item;
-
-public interface IMakeItem {
-
-    Item getItem();
-
-}
--- MissysDelight-master/src/main/java/thearknoah05/missy/proxy/IProxy.java
+++
@@ -1,7 +0,0 @@
-package thearknoah05.missy.proxy;
-
-public interface IProxy {
-
-    default void init() {}
-
-}
